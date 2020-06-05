26e2e42df369d57d68d06a3b3f891e9d8d0079a3
tnok_it_office_893 <48271049+massarfusion@users.noreply.github.com>
2020-06-05 15:59:51 +0800

    Add files via upload

--- DialBook.asm
+++ DialBook.asm
@@ -1,18 +1,23 @@
 ; multi-segment executable file template.
 
 data segment
-    welcome db "1 for adding,2 for comsulting,3 for displaying.4 for exit.$"
+    welcome db "1 for adding,2 for consulting,3 for displaying.4 for exit.$"
     order db 0
+    overflowprompt db "It is over the limit.Returning to title....$"
+    displayover db "Display is over.Returning to title$"
+    nonedisplay db "No item here,Please add some.Returning to title...$"
     pkey db "press any key to exit...$"
     initialwrong1 db "I cannot understand.Please give a correct order$"
     addnameprompt db "input a name to save,below 20 words$"
     addnumberprompt db "input a number under 8 words$"
-    consultnameprompt db "input a name to consult$"
+    consultnameprompt db "input a name to consult: $"
     consultnumberprompt db "input a number to consult$"
     totitlebuf db "Returning to title...$"
     consultfailureprompt db "No result found.Return to menu...$"
     consultsuccessprompt db "Record found,Number is:$"
+    ifempty db 1
     pool db 300 dup(0)
+    savednum db 0
     addnamebuf db 21,0,21 dup(0)
     addnumberbuf db 9,0,9 dup(0)
     deletenamebuf db 21,0,21 dup(0)
@@ -73,6 +78,7 @@ ret
 ;;
 adder:
 call reline
+call adder_numcheck
 add bp,30
 mov si,bp;作为基准的地址数值。
 ;cleaner:mov byte ptr pool[si],0
@@ -92,11 +98,10 @@ mov bx,-1
 s1:inc bx
 cmp byte ptr addnamebuf[bx+2],0dh
 jne s1;定位到键入信息的回车键之后
-
 fillzero:mov byte ptr addnamebuf[bx+2],0
 inc bx
 cmp bx,20
-jne fillzero;用0补完空位
+jne fillzero;用0补完空位,免得本次的数据写入不能完全覆盖上次的。
 mov byte ptr addnamebuf[bx+2],0dh
 mov cx,20
 mov bx,-1
@@ -104,7 +109,7 @@ store:inc bx
 mov dl,byte ptr addnamebuf[bx+2]
 mov byte ptr pool[si+bx],dl
 loop store
-mov byte ptr pool[si+20],'$'
+mov byte ptr pool[si+bx+1],'$';封盖，方便输出
 call reline
 mov dx,offset addnumberprompt
 mov ah,9
@@ -126,13 +131,32 @@ inc bx
 mov dl,byte ptr addnumberbuf[bx+2]
 mov byte ptr pool[si+bx],dl
 loop storenumber
-mov byte ptr pool[si+8],'$'
+mov byte ptr pool[si+bx+1],'$'
 call reline
 mov dx,offset totitlebuf
 mov ah,9
 int 21h
 call reline
+inc savednum
+mov ifempty,0;表示非空
 jmp start
+
+;;;;;;;
+adder_numcheck:
+cmp savednum,10
+jge start_overflow
+ret
+start_overflow:
+push ax
+push dx
+mov dx , offset overflowprompt
+mov ah,9
+int 21h
+pop dx
+pop ax
+jmp start
+;;;;;;;
+;;;;;;;
 ;;;;;;;
 consult:
 mov dx,offset consultnameprompt
@@ -174,7 +198,6 @@ cmpsb
 jnz outer
 loop inner
 jmp success
-
 fail:
 mov dx,offset consultfailureprompt
 mov ah,9
@@ -182,7 +205,6 @@ int 21h
 call reline
 pop currentindex
 jmp start
-
 success:
 mov dx,offset consultsuccessprompt
 mov ah,9
@@ -200,6 +222,46 @@ jmp start
 
 ;;;;;;;
 display:
+call reline
+cmp ifempty,1
+je emptyexc
+mov bx,0
+mov bp,offset pool
+sub bp,30
+circle:
+push bp
+add bp,30
+sub bp,offset pool
+cmp bp,currentindex
+jg jumpout
+pop bp
+add bp,30
+mov dx,bp
+mov ah,9
+int 21h
+call reline
+add dx,21
+mov ah,9
+int 21h
+call reline
+jmp circle
+;circle
+emptyexc:
+push dx
+push ax
+mov dx,offset nonedisplay;无记录的错误提示
+mov ah,9
+int 21h
+call reline
+pop ax
+pop dx
+jmp start
+jumpout:
+mov dx,offset displayover
+mov ah,9
+int 21h
+call reline
+jmp start
 
 jmp start
 ;;;;;;;
